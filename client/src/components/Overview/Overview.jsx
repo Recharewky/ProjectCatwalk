fef56483384b30b1743128448bbc14bcc7d7b2ad
kyle_sablan <kyle.sablan@gmail.com>
2021-10-27 11:51:12 -0700

    Added basic react class component for product overview

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,1 +1,18 @@
 import React from 'react';
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        ~This is where te overview would go~
+        Check out this cool product
+      </div>
+    )
+  }
+}
+
+export default Overview;

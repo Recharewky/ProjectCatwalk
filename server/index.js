bc1eab9bcf26efe61bf828aa4caa67bd5669307e
Andrew Vaughan <liljazzman@Andrews-MacBook-Pro.local>
2021-10-27 09:07:54 -0700

    Initial Commit. Webpack needs work

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const axios = require('axios');
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('client/public'));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+})
+
+module.export = app;

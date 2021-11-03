16034781dba3c8d3ca2b47be9913ed3e9ad21747
kyle_sablan <kyle.sablan@gmail.com>
2021-11-03 15:52:13 -0700

    Added initial 'gallery' view of thumbnails on the side

--- client/src/components/Overview/MainImage.jsx
+++ client/src/components/Overview/MainImage.jsx
@@ -2,29 +2,56 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const Main = styled.img`
+  height: 25em;
+  width: 30em;
+  object-fit: cover;
+  border-radius: 14px;
+  border: 5px solid orange;
+}
+`;
+
+const Thumbnail = styled.img`
+  height: 3em;
+  width: 3em;
+  object-fit: contain;
+  border-radius: 5px;
+  border: 3px solid blue;
+  background-color: darkgray;
+}
+`;
+
+const LeftArrow = styled.button`
+  height: 3em;
+  width: 3em;
+  background-color: green;
+  border-radius: 5px;
+`;
+
+const RightArrow = styled.button`
+  height: 3em;
+  width: 3em;
+  background-color: green;
+  border-radius: 5px;
+`;
+
+const Gallery = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+// const
+
 const MainImage = ({ photos }) => {
   const [currentPhoto, setPhoto] = useState(0);
 
-  const Main = styled.img`
-    height: 25em;
-    width: 30em;
-    object-fit: cover;
-  `;
-
-  const LeftArrow = styled.button`
-    height: 5em;
-    width: 5em;
-    background-color: white;
-  `;
-
-  const RightArrow = styled.button`
-    height: 5em;
-    width: 5em;
-    background-color: white;
-  `;
+  const Thumbnails = photos.map((photo) => <Thumbnail src={photo.thumbnail_url} alt="thumbnail for product" />);
 
   return (
     <div>
+      <Gallery>
+        {Thumbnails}
+      </Gallery>
       <Main src={photos[currentPhoto].url} alt="hi-res product" />
       <LeftArrow
         type="button"

495d74ee748c2df1adc7b2f41c0440926e0c3e75
kyle_sablan <kyle.sablan@gmail.com>
2021-11-02 20:44:46 -0700

    Added accurate functionality to selection drop down menus

--- client/src/components/Overview/Selection.jsx
+++ client/src/components/Overview/Selection.jsx
@@ -3,22 +3,41 @@ import React, { useState } from 'react';
 
 const Selection = ({ currentStyle }) => {
   // const [selectedSize, setSize] = useState('Select Size');
-  // const [selectedAmt, setAmt] = useState('-');
+  const [amtEnabled, enableAmt] = useState(true);
+
+  const amtOptions = [<option value="amt" selected disabled>-</option>];
+  const [availableAmt, setAmt] = useState(amtOptions);
+
   const sizeOptions = [<option value="size" selected disabled>Select Size</option>];
-  const amtOptions = [];
+  const stockPerSizes = [];
   const skus = Object.values(currentStyle.skus);
   let i;
   for (i = 0; i < skus.length; i += 1) {
     sizeOptions.push(<option value={skus[i].size}>{skus[i].size}</option>);
-    amtOptions.push(<option value={skus[i].quantity}>{skus[i].quantity}</option>);
+    stockPerSizes.push(skus[i].quantity);
   }
   return (
     <div>
-      <select>
+      <select onChange={(e) => {
+        enableAmt(false);
+        console.log(e.target.selectedIndex - 1);
+        console.log(stockPerSizes[e.target.selectedIndex - 1]);
+        const amountList = [];
+        const choice = e.target.selectedIndex - 1;
+        let max = stockPerSizes[choice];
+        if (max > 15) {
+          max = 15;
+        }
+        for (let j = 1; j <= max; j += 1) {
+          amountList.push(<option value={j}>{j}</option>);
+        }
+        setAmt(amountList);
+      }}
+      >
         {sizeOptions}
       </select>
-      <select disabled>
-        {amtOptions}
+      <select disabled={amtEnabled}>
+        {availableAmt}
       </select>
     </div>
   );

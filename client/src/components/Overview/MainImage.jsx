2e4c690460dd4d528b48231fa55d881e79c5c506
kyle_sablan <kyle.sablan@gmail.com>
2021-11-03 14:22:26 -0700

    Added basic styling to maintain image sizing

--- client/src/components/Overview/MainImage.jsx
+++ client/src/components/Overview/MainImage.jsx
@@ -1,8 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import styled from 'styled-components';
 
-const MainImage = ({ photos }) => (
-  <img src={photos[0].url} alt="hi-res product" />
-);
+const MainImage = ({ photos }) => {
+  const Main = styled.img`
+    height: 25em;
+    width: 30em;
+    object-fit: cover;
+  `;
+
+  return (
+    <Main src={photos[0].url} alt="hi-res product" />
+  );
+};
 
 export default MainImage;

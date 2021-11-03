8cbc6772d1a51ffdcbf6af9d8987bfdeb8c91cbb
kyle_sablan <kyle.sablan@gmail.com>
2021-11-03 15:16:19 -0700

    Added in basic switching among available imaged per product & style

--- client/src/components/Overview/MainImage.jsx
+++ client/src/components/Overview/MainImage.jsx
@@ -1,16 +1,52 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const MainImage = ({ photos }) => {
+  const [currentPhoto, setPhoto] = useState(0);
+
   const Main = styled.img`
     height: 25em;
     width: 30em;
     object-fit: cover;
   `;
 
+  const LeftArrow = styled.button`
+    height: 5em;
+    width: 5em;
+    background-color: white;
+  `;
+
+  const RightArrow = styled.button`
+    height: 5em;
+    width: 5em;
+    background-color: white;
+  `;
+
   return (
-    <Main src={photos[0].url} alt="hi-res product" />
+    <div>
+      <Main src={photos[currentPhoto].url} alt="hi-res product" />
+      <LeftArrow
+        type="button"
+        onClick={() => {
+          if (currentPhoto > 0) {
+            setPhoto(currentPhoto - 1);
+          }
+        }}
+      >
+        {'<'}
+      </LeftArrow>
+      <RightArrow
+        type="button"
+        onClick={() => {
+          if (currentPhoto < photos.length - 1) {
+            setPhoto(currentPhoto + 1);
+          }
+        }}
+      >
+        {'>'}
+      </RightArrow>
+    </div>
   );
 };
 

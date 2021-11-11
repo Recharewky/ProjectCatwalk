a143eea6248dae7d255e00d1850c6a6aacc071e3
kyle_sablan <kyle.sablan@gmail.com>
2021-11-11 14:13:43 -0800

    Connected client app to server

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,15 +1,13 @@
 /* eslint-disable import/extensions */
 /* eslint-disable react/prop-types */
 import React from 'react';
-
-// import axios from 'axios';
+import axios from 'axios';
 import styled from 'styled-components';
 import ProductInfo from './ProductInfo.jsx';
 import MainImage from './MainImage.jsx';
 import StyleOptions from './StyleOptions.jsx';
 import Description from './Description.jsx';
 import Selection from './Selection.jsx';
-import dummyData from '../../../../dummyData.js';
 
 const OverviewContainer = styled.div`
   display: inline-flex;
@@ -21,40 +19,63 @@ class Overview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      info: dummyData.product,
-      styles: dummyData.styles,
-      currentStyle: dummyData.styles.results[0],
+      info: null,
+      styles: null,
+      currentStyle: null,
     };
 
     this.onStyleChange = this.onStyleChange.bind(this);
+  }
+
+  componentDidMount() {
+    const { id } = this.props;
+    axios.get(`/products/${id}`)
+      .then((response1) => {
+        const productResponse = response1.data;
+        axios.get(`/products/${id}/styles`)
+          .then((response2) => {
+            const styleResponse = response2.data;
+            this.setState({
+              info: productResponse,
+              styles: styleResponse,
+              currentStyle: styleResponse.results[0],
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
   }
 
   onStyleChange(e, style) {
     this.setState({
       currentStyle: style,
     });
-
   }
 
   render() {
     const { info, styles, currentStyle } = this.state;
     return (
       <div>
-        <OverviewContainer>
-          <MainImage photos={currentStyle.photos} />
+        {info ? (
           <div>
-            <ProductInfo info={info} />
-            <StyleOptions
-              styles={styles}
-              currentStyle={currentStyle}
-              onStyleChange={this.onStyleChange}
-            />
+            <OverviewContainer>
+              <MainImage photos={currentStyle.photos} />
+              <div>
+                <ProductInfo info={info} />
+                <StyleOptions
+                  styles={styles}
+                  currentStyle={currentStyle}
+                  onStyleChange={this.onStyleChange}
+                />
+              </div>
+              <div>
+                <Selection currentStyle={currentStyle} />
+              </div>
+            </OverviewContainer>
+            <Description info={info} />
           </div>
-          <div>
-            <Selection currentStyle={currentStyle} />
-          </div>
-        </OverviewContainer>
-        <Description info={info} />
+        ) : null}
       </div>
     );
   }
